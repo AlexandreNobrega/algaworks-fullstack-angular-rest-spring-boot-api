13845cd6a1e0604e466176c3e7b6a07a7b4b56e1
Alexandre <alexandrenobrega777@gmail.com>
2021-01-30 19:35:54 -0300

    Criando Eventos para recursos

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 
@@ -46,6 +49,9 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	/*
 	 * Realizando o mapeamento para o recurso "/pessoas", através da anotação @GetMapping para listar as pessoas
 	 * @return
@@ -64,10 +70,10 @@ public class PessoaResource {
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-			.buildAndExpand(pessoa.getCodigo()).toUri();
 		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
