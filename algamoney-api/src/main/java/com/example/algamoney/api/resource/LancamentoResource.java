f8cb708c5145c010867326ccff24d5733d93b3bc
Alexandre Nobrega <alexandrenobrega777@gmail.com>
2021-02-09 11:21:54 -0300

    Criado Busca Pelo Codigo de Lancamento

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -1,12 +1,15 @@
 package com.example.algamoney.api.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,16 @@ public class LancamentoResource {
     public List<Lancamento> listar() {
         return lancamentoRepository.findAll();
     }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) { 
+        Optional<Lancamento> lancamento = this.lancamentoRepository.findById(codigo);
+
+        if (lancamento.isPresent()){
+            return ResponseEntity.ok(lancamento.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
